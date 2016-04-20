65f492f66de91089f6ad61791b8e3cc2b24a1987
Jelard Macalino <jmacalino@Epitecs-MacBook-Pro-2.local>
2016-04-20 14:25:00 -0400

    Used template insteadof template url

--- src/epi-documents-library.component.ts
+++ src/epi-documents-library.component.ts
@@ -2,7 +2,31 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector:'epi-documents-library',
-    templateUrl: 'src/epi-documents-library.component.html'
+    template:`
+    <div class="widget-header">
+    <h3>{{title}}</h3>
+    </div>
+    <div class="widget-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="icon">No.</th>
+                    <th style="width: 300px;">Type</th>
+                    <th>Current document</th>
+                    <th style="width: 100px;">Created on</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr *ngFor="#documentType of documentTypes" >
+                    <td>{{documentType.numberOfDocuments}}</td>
+                    <td>{{documentType.name}} </td>
+                    <td>{{documentType.latestDocumentName}}</td>
+                    <td>{{documentType.latestDocumentCreatedDate|date}}</td>               
+                </tr>
+            </tbody>
+        </table>
+    </div>    
+    ` 
 })
 export class EpiDocumentsLibraryComponent{
     title:string = 'Documents Library';
